255b03a5b811176ce3b9dfde54449400cef12d5c
Sample18 <Sample417@yandex.ru>
2024-03-20 00:30:29 +0300

    create spring mvc

---
+++ src/main/java/web/dao/CarDAO.java
@@ -0,0 +1,31 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CarDAO {
+    private List<Car> cars;
+
+    private int ID;
+
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car(++ID, "BMW", 3));
+        cars.add(new Car(++ID, "Audi", 2));
+        cars.add(new Car(++ID, "Lexus", 1));
+        cars.add(new Car(++ID, "Ford", 5));
+        cars.add(new Car(++ID, "Kia", 7));
+    }
+
+    public List<Car> getCars(int count) {
+        return cars.stream().limit(count).toList();
+    }
+
+    public List<Car> getAllCars() {
+        return cars;
+    }
+}
